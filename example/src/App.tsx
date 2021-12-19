754e84e11c4ccc7cc1a3724331213519fbe58158
Trex <thiensmail96@gmail.com>
2021-12-19 19:43:01 +0700

    add: BackdropView component

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,15 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-overlay';
+// import { BackDropView } from 'react-native-overlay';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
+  const [result] = React.useState<number | undefined>();
 
   return (
     <View style={styles.container}>
       <Text>Result: {result}</Text>
+      {/*<BackDropView animatedProgress={0} />*/}
     </View>
   );
 }
