7f7e4f992226e53160b2c2b043c2a6da45d0df21
Trex <thiensmail96@gmail.com>
2021-12-26 20:19:51 +0700

    add: animated configuration as constant

---
+++ src/constants.ts
@@ -0,0 +1,152 @@
+import {
+  Easing,
+  runOnJS,
+  withSpring,
+  withTiming,
+  interpolate,
+} from 'react-native-reanimated';
+import type { AnimatedConfiguration } from './types';
+
+const BaseDuration = 250;
+
+export const SpringConfiguration = {
+  mass: 1,
+  damping: 60,
+  velocity: 0,
+  stiffness: 280,
+  overshootClamping: false,
+  restSpeedThreshold: 0.001,
+  restDisplacementThreshold: 0.001,
+};
+
+export const FadeConfiguration: AnimatedConfiguration = {
+  style: (progress) => {
+    'worklet';
+    return { opacity: progress.value };
+  },
+  present: (progress, onFinished) => {
+    progress.value = withTiming(
+      1,
+      { duration: BaseDuration, easing: Easing.ease },
+      (isFinished) => {
+        if (isFinished) {
+          runOnJS(onFinished)();
+        }
+      }
+    );
+  },
+  dismiss: (progress, onFinished) => {
+    progress.value = withTiming(
+      0,
+      { duration: BaseDuration, easing: Easing.ease },
+      () => {
+        runOnJS(onFinished)();
+      }
+    );
+  },
+};
+
+export const ScaleConfiguration: AnimatedConfiguration = {
+  style: (progress) => {
+    'worklet';
+    return { transform: [{ scale: progress.value }] };
+  },
+  present: (progress, onFinished) => {
+    progress.value = withSpring(1, SpringConfiguration, (isFinished) => {
+      if (isFinished) {
+        runOnJS(onFinished)();
+      }
+    });
+  },
+  dismiss: (progress, onFinished) => {
+    progress.value = withTiming(
+      0,
+      { duration: BaseDuration, easing: Easing.ease },
+      (isFinished) => {
+        if (isFinished) {
+          runOnJS(onFinished)();
+        }
+      }
+    );
+  },
+};
+
+export const SlideUpAnimation: AnimatedConfiguration = {
+  style: (progress, extraInfo) => {
+    'worklet';
+    const { height, offsetY } = extraInfo.layout.value;
+    return {
+      opacity: interpolate(progress.value, [0, 1], [0, 1]),
+      transform: [
+        {
+          translateY: interpolate(progress.value, [0, 1], [height, offsetY]),
+        },
+      ],
+    };
+  },
+  present: (progress, onFinished) => {
+    progress.value = withSpring(1, SpringConfiguration, (isFinished) => {
+      if (isFinished) {
+        runOnJS(onFinished)();
+      }
+    });
+  },
+  dismiss: (progress, onFinished) => {
+    progress.value = withTiming(
+      0,
+      { duration: BaseDuration, easing: Easing.ease },
+      (isFinished) => {
+        if (isFinished) {
+          runOnJS(onFinished)();
+        }
+      }
+    );
+  },
+};
+
+export const SlideDownConfiguration: AnimatedConfiguration = {
+  style: (progress, extraInfo) => {
+    'worklet';
+    const { height } = extraInfo.layout.value;
+    //
+    return {
+      opacity: interpolate(progress.value, [0, 1], [0, 1]),
+      transform: [
+        {
+          translateY: interpolate(progress.value, [0, 1], [-height, 0]),
+        },
+      ],
+    };
+  },
+  present: (progress, onFinished) => {
+    progress.value = withSpring(1, SpringConfiguration, (isFinished) => {
+      if (isFinished) {
+        runOnJS(onFinished)();
+      }
+    });
+  },
+  dismiss: (progress, onFinished) => {
+    progress.value = withTiming(
+      0,
+      { duration: BaseDuration, easing: Easing.ease },
+      (isFinished) => {
+        if (isFinished) {
+          runOnJS(onFinished)();
+        }
+      }
+    );
+  },
+};
+
+export const NoneConfiguration: AnimatedConfiguration = {
+  style: (_) => {
+    'worklet';
+    return {};
+  },
+  present: (_, onFinished) => {
+    runOnJS(onFinished)();
+  },
+  dismiss: (_, onFinished) => {
+    runOnJS(onFinished)();
+  },
+};
