7e48fd220573a348121483c897286b2816b687b1
Trex <thiensmail96@gmail.com>
2021-12-28 19:59:44 +0700

    chore: update slidelayer index.ts

--- src/components/slidelayer/index.ts
+++ src/components/slidelayer/index.ts
@@ -1,1 +1,5 @@
 export * from './SlideUpContent';
+export * from './SlideDownContent';
+export * from './SlideUpInputContent';
+export * from './SlideFromLeftContent';
+export * from './SlideFromRightContent';
