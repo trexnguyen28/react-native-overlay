e1e11521f7bec55366490dcb33093d220ae7db3b
Trex <thiensmail96@gmail.com>
2021-12-28 19:28:28 +0700

    Update: animated configuration constants

--- src/constants.ts
+++ src/constants.ts
@@ -107,13 +107,79 @@ export const SlideUpAnimation: AnimatedConfiguration = {
 export const SlideDownConfiguration: AnimatedConfiguration = {
   style: (progress, extraInfo) => {
     'worklet';
-    const { height } = extraInfo.layout.value;
+    const { height, offsetY } = extraInfo.layout.value;
     //
     return {
       opacity: interpolate(progress.value, [0, 1], [0, 1]),
       transform: [
         {
-          translateY: interpolate(progress.value, [0, 1], [-height, 0]),
+          translateY: interpolate(progress.value, [0, 1], [-height, -offsetY]),
+        },
+      ],
+    };
+  },
+  present: (progress, onFinished) => {
+    progress.value = withSpring(1, SpringConfiguration, (isFinished) => {
+      if (isFinished) {
+        runOnJS(onFinished)();
+      }
+    });
+  },
+  dismiss: (progress, onFinished) => {
+    progress.value = withTiming(
+      0,
+      { duration: BaseDuration, easing: Easing.ease },
+      (isFinished) => {
+        if (isFinished) {
+          runOnJS(onFinished)();
+        }
+      }
+    );
+  },
+};
+
+export const SlideFromLeftConfiguration: AnimatedConfiguration = {
+  style: (progress, extraInfo) => {
+    'worklet';
+    const { width, offsetX } = extraInfo.layout.value;
+    return {
+      opacity: interpolate(progress.value, [0, 1], [0, 1]),
+      transform: [
+        {
+          translateX: interpolate(progress.value, [0, 1], [-width, -offsetX]),
+        },
+      ],
+    };
+  },
+  present: (progress, onFinished) => {
+    progress.value = withSpring(1, SpringConfiguration, (isFinished) => {
+      if (isFinished) {
+        runOnJS(onFinished)();
+      }
+    });
+  },
+  dismiss: (progress, onFinished) => {
+    progress.value = withTiming(
+      0,
+      { duration: BaseDuration, easing: Easing.ease },
+      (isFinished) => {
+        if (isFinished) {
+          runOnJS(onFinished)();
+        }
+      }
+    );
+  },
+};
+
+export const SlideFromRightConfiguration: AnimatedConfiguration = {
+  style: (progress, extraInfo) => {
+    'worklet';
+    const { width, offsetX } = extraInfo.layout.value;
+    return {
+      opacity: interpolate(progress.value, [0, 1], [0, 1]),
+      transform: [
+        {
+          translateX: interpolate(progress.value, [0, 1], [width, offsetX]),
         },
       ],
     };
